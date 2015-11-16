5359110f1f6ba2c761a731ff7e30bb1663cf1af4
Kazuma SATO <ranceworks@gmail.com>
2015-11-17 07:37:30 +0900

    create skeleton #2

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rake', '10.4.2'
+gem 'matrix'
+gem 'tf-idf-similarity'
---
+++ kyo.rb
@@ -0,0 +1,9 @@
+module Jekyll
+
+	class RelatedPostSuggester < Generator
+		safe true
+
+		def generate(site)
+		end
+	end
+end
