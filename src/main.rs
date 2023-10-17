81c5cf6a79f80b2b2b0ce528c2b6ec9c56d28c6c
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-10-17 00:09:17 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,44 @@
-// use gtk::prelude::*;
-// use gtk::{glib, Application, ApplicationWindow};
-// use gtk4 as gtk;
-
-// fn main() -> glib::ExitCode {
+// use gtk4::builders::ApplicationBuilder;
+// use gtk4::prelude::*;
+// use gtk4::{Application, Builder};
+// fn main() {
+//     let application: ApplicationBuilder = Application::builder();
+//     let builder = Builder::new();
+//     builder.add_from_file("a.ui");
 //     let app = Application::builder()
 //         .application_id("org.example.HelloWorld")
 //         .build();
-
 //     app.connect_activate(|app| {
-//         // We create the main window.
-//         let window = ApplicationWindow::builder()
-//             .application(app)
-//             .default_width(320)
-//             .default_height(200)
-//             .title("Hello, World!")
-//             .build();
-
-//         // Show the window.
-//         window.present();
+//         //builder.object("window");
 //     });
-
-//     app.run()
+//     app.run();
+//     println!("Hello, world!");
 // }
 
-//use gtk4 as gtk;
-use gtk4::builders::ApplicationBuilder;
 use gtk4::prelude::*;
-use gtk4::{Application, Builder};
+use gtk4::{glib, Application, ApplicationWindow, Builder};
+use gtk4_layer_shell::{Layer, LayerShell};
+
+//fn main() -> glib::ExitCode {
 fn main() {
-    let application: ApplicationBuilder = Application::builder();
-    let builder = Builder::new();
-    builder.add_from_file("a.ui");
+    // Create a new application
     let app = Application::builder()
-        .application_id("org.example.HelloWorld")
+        .application_id("dev.nryotaro.kanami")
         .build();
-    app.connect_activate(|app| {
-        builder.object("window");
-    });
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
     app.run();
-    println!("Hello, world!");
+}
+
+fn build_ui(app: &Application) {
+    // https://stackoverflow.com/questions/66942543/how-do-we-build-gui-with-glade-gtk-rs-in-rust
+    let builder = Builder::new();
+    builder.add_from_file("src/system.ui").expect("fail");
+    let window: ApplicationWindow = builder.object("window").expect("fail2");
+    window.init_layer_shell();
+    window.set_layer(Layer::Overlay);
+    window.show();
 }
