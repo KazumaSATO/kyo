dd3e0d786236776a475cd5940ae401efc5afc231
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-11-02 21:17:44 -0400

    hey

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-// use gtk4::builders::ApplicationBuilder;
-// use gtk4::prelude::*;
-// use gtk4::{Application, Builder};
-// fn main() {
-//     let application: ApplicationBuilder = Application::builder();
-//     let builder = Builder::new();
-//     builder.add_from_file("a.ui");
-//     let app = Application::builder()
-//         .application_id("org.example.HelloWorld")
-//         .build();
-//     app.connect_activate(|app| {
-//         //builder.object("window");
-//     });
-//     app.run();
-//     println!("Hello, world!");
-// }
-//use gdk4::prelude::*;
 use gdk4::Display;
+
+use glib::{clone, source, ControlFlow, MainContext};
 use gtk4::prelude::*;
 use gtk4::{
-    Application, ApplicationWindow, Builder, CallbackAction, CssProvider, ListBox, Shortcut,
+    glib, Application, ApplicationWindow, Builder, CallbackAction, CssProvider, ListBox, Shortcut,
     ShortcutController, ShortcutTrigger,
 };
 use gtk4_layer_shell::{Edge, KeyboardMode, Layer, LayerShell};
@@ -46,32 +31,55 @@ fn build_ui(app: &Application) {
     builder.add_from_file("src/system.ui").expect("fail");
     let window: ApplicationWindow = builder.object("window").expect("fail2");
     let list: ListBox = builder.object("list").expect("fail3");
-    let a = 3;
-    list.connect_row_activated(move |list_box, option_list_box| {
+    list.connect_row_activated(move |_list_box, _option_list_box| {
         println!("doge");
-        a;
     });
     // Prevent the window from closing just after the application starts.
     window.set_application(Some(app));
     //window.set_opacity(0.1);
     window.init_layer_shell();
     window.set_keyboard_mode(KeyboardMode::OnDemand);
-    window.set_layer(Layer::Overlay);
+    //window.set_layer(Layer::Overlay);
+    window.set_layer(Layer::Top);
     // window.set_margin(Edge::Left, 200);
     // window.set_anchor(Edge::Left, true);
     // Boxは非対称のサイズに子供を指定できるみたい。
     let shortcut_controller = ShortcutController::new();
     let trigger = ShortcutTrigger::parse_string("Escape").unwrap();
+
+    let (sender, receiver) = glib::MainContext::channel::<i32>(glib::source::Priority::DEFAULT);
+
     let shortcut = Shortcut::builder()
         .trigger(&trigger)
         .action(&CallbackAction::new(move |_, b| {
-            println!("doge");
+            println!("kemo");
+            sender.send(1).unwrap();
             true
         }))
         .build();
+
     shortcut_controller.add_shortcut(shortcut);
     window.add_controller(shortcut_controller);
-    window.show();
+    window.present();
+    // main_context.spawn_local(clone!(@weak window => async move {
+    // //
+    //         while let Ok(res) = receiver.recv() {
+    //             println!("foobar {res}");
+    //             if res == 1 {
+    //                 //window.close();
+    //             } else {
+
+    //                 println!("doge2");
+
+    //             }
+    //         }
+    receiver.attach(None, move |x| {
+        window.close();
+        glib::ControlFlow::Continue
+    });
+
+    println!("present");
+    //window.show();
 }
 
 fn load_css() {
