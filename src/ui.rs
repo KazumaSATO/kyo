1b255526fdf9a1bc239e50f06bc398390c0a6f3f
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-11-23 03:05:17 -0500

    a

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,14 @@
 use crate::config::{Command, Config};
 use gdk4::Texture;
+use gtk4::glib::signal::Propagation;
 use gtk4::glib::source::Priority;
 use gtk4::glib::{Bytes, ControlFlow, MainContext, Receiver, Sender};
 use gtk4::prelude::*;
 use gtk4::{
-    Application, CallbackAction, DirectionType, GestureClick, Image, Label, ListBox, ListBoxRow,
-    Orientation, Shortcut, ShortcutController, ShortcutTrigger, Window,
+    Application, CallbackAction, DirectionType, EventControllerKey, GestureClick, Image, Label,
+    ListBox, ListBoxRow, Orientation, Shortcut, ShortcutController, ShortcutTrigger, Window,
 };
+
 use gtk4_layer_shell::{KeyboardMode, Layer, LayerShell};
 //pub mod config;
 
@@ -14,19 +16,14 @@ pub fn build_ui(app: &Application, config: &Config) {
     let window = build_window(app);
     let list_box = build_list(config);
     window.set_child(Some(&list_box));
-
     // let (sender, receiver): (Sender<Message>, Receiver<Message>) =
     //     MainContext::channel::<Message>(Priority::DEFAULT);
-
-    // let shortcut_controller = build_shortcut_controller(sender);
-    // window.add_controller(shortcut_controller);
     window.present();
     // receiver.attach(None, move |msg| {
     //     match msg {
     //         Message::CloseWindow => window.close(),
     //         Message::Move(direction) => list_box.emit_move_focus(direction),
     //     }
-
     //     ControlFlow::Continue
     // });
 }
@@ -84,6 +81,7 @@ fn build_list(config: &Config) -> ListBox {
         .action(&ctrl_p_action)
         .build();
     shortcut_controller.add_shortcut(ctrl_p_shortcut);
+
     let ctrl_n_trigger = ShortcutTrigger::parse_string("<Control>n").unwrap();
     let ctrl_n_action = CallbackAction::new(move |widget, _| {
         let list_box: &ListBox = widget.downcast_ref().unwrap();
@@ -95,9 +93,17 @@ fn build_list(config: &Config) -> ListBox {
         .action(&ctrl_n_action)
         .build();
     shortcut_controller.add_shortcut(ctrl_n_shortcut);
+
+    let controller_key = EventControllerKey::new();
+    controller_key.connect_key_pressed(|k, key, c, modifier_type| {
+        println!("hey, {}, {}", key, c);
+        Propagation::Proceed
+    });
+    list_box.add_controller(controller_key);
     list_box.add_controller(shortcut_controller);
     list_box
 }
+
 fn build_entry(label: &str, icon: &str, command: &Command) -> gtk4::Box {
     let bx = gtk4::Box::new(Orientation::Horizontal, 16);
     let bytes = Bytes::from(icon.as_bytes());
@@ -115,71 +121,5 @@ fn build_entry(label: &str, icon: &str, command: &Command) -> gtk4::Box {
     });
     bx.set_property("name", "entry");
     bx.add_controller(click_controller);
-
     bx
-}
-
-fn build_shortcut_controller<'a>(sender: Sender<Message>) -> ShortcutController {
-    let shortcut_controller = ShortcutController::new();
-    shortcut_controller.add_shortcut(build_shortcut_escape(sender.clone()));
-    //shortcut_controller.add_shortcut(build_shortcut_ctrlp(sender.clone()));
-    shortcut_controller
-}
-
-enum Message {
-    CloseWindow,
-    Move(DirectionType),
-}
-fn build_shortcut_escape(sender: Sender<Message>) -> Shortcut {
-    let trigger = ShortcutTrigger::parse_string("Escape").unwrap();
-    let action = CallbackAction::new(move |_, _| {
-        sender.send(Message::CloseWindow).unwrap();
-        true
-    });
-    let builder = Shortcut::builder().trigger(&trigger).action(&action);
-    builder.build()
-}
-fn build_shortcut_ctrlp(sender: Sender<Message>) -> Shortcut {
-    let trigger = ShortcutTrigger::parse_string("<Control>p").unwrap();
-    let action = CallbackAction::new(move |_, _| {
-        sender.send(Message::Move(DirectionType::Up)).unwrap();
-        true
-    });
-    let builder = Shortcut::builder().trigger(&trigger).action(&action);
-    builder.build()
-}
-
-trait Selectable {
-    fn prev(&self);
-
-    fn next(&self);
-
-    fn selected_size(&self) -> (i32, i32);
-}
-impl Selectable for ListBox {
-    fn prev(&self) {
-        let (i, _) = self.selected_size();
-        if i > 0 {
-            self.select_row(Some(&self.row_at_index(i - 1).unwrap()));
-        }
-    }
-    fn next(&self) {
-        let (i, size) = self.selected_size();
-        if i < size - 1 {
-            self.select_row(Some(&self.row_at_index(i + 1).unwrap()));
-        }
-    }
-
-    fn selected_size(&self) -> (i32, i32) {
-        let mut count = 0;
-        let mut i = 0;
-        let selected = self.selected_row().unwrap();
-        while let Some(row) = self.row_at_index(count) {
-            if row == selected {
-                i = count;
-            }
-            count += 1;
-        }
-        (i, count)
-    }
 }
