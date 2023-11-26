914db0bf4029ef3a91b419545422375d14b46321
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-11-25 19:47:15 -0500

    shortcut

--- src/ui.rs
+++ src/ui.rs
@@ -1,13 +1,18 @@
 use crate::config::{Command, Config};
 use gdk4::Texture;
+use gio::ActionEntry;
+use glib::clone;
 use gtk4::glib::signal::Propagation;
 use gtk4::glib::source::Priority;
 use gtk4::glib::{Bytes, ControlFlow, MainContext, Receiver, Sender};
 use gtk4::prelude::*;
 use gtk4::{
-    Application, CallbackAction, DirectionType, EventControllerKey, GestureClick, Image, Label,
-    ListBox, ListBoxRow, Orientation, Shortcut, ShortcutController, ShortcutTrigger, Window,
+    Application, ApplicationWindow, CallbackAction, DirectionType, EventControllerKey,
+    GestureClick, Image, Label, ListBox, ListBoxRow, Orientation, Shortcut, ShortcutController,
+    ShortcutTrigger, Window,
 };
+use std::cell::{Cell, RefCell};
+use std::rc::Rc;
 
 use gtk4_layer_shell::{KeyboardMode, Layer, LayerShell};
 //pub mod config;
@@ -28,10 +33,10 @@ pub fn build_ui(app: &Application, config: &Config) {
     // });
 }
 
-fn build_window(app: &Application) -> Window {
+fn build_window(app: &Application) -> ApplicationWindow {
     // https://stackoverflow.com/questions/66942543/how-do-we-build-gui-with-glade-gtk-rs-in-rust
     // https://github.com/pachi/visol/blob/master/src/window.rs
-    let builder = Window::builder();
+    let builder = ApplicationWindow::builder();
     let window = builder.application(app).build();
     window.init_layer_shell();
     window.set_keyboard_mode(KeyboardMode::OnDemand);
@@ -41,19 +46,26 @@ fn build_window(app: &Application) -> Window {
     // window.set_margin(Edge::Left, 200);
     // window.set_anchor(Edge::Left, true);
     // Boxは非対称のサイズに子供を指定できるみたい。
-    //builder.add_from_string(include_str!("system.ui")).unwrap();
     window.set_property("name", "window");
     let trigger = ShortcutTrigger::parse_string("Escape").unwrap();
     let action = CallbackAction::new(move |widget, _| {
-        let window: &Window = widget.downcast_ref().unwrap();
-        window.close();
+        widget.activate_action("win.close", None).unwrap();
+        // let window: &ApplicationWindow = widget.downcast_ref().unwrap();
+        // window.close();
         true
     });
     let builder = Shortcut::builder().trigger(&trigger).action(&action);
     let controller = ShortcutController::new();
     controller.add_shortcut(builder.build());
-
     window.add_controller(controller);
+
+    // Add action "close" to `window` taking no parameter
+    let action_close: ActionEntry<ApplicationWindow> = ActionEntry::builder("close")
+        .activate(|window: &ApplicationWindow, _, _| {
+            window.close();
+        })
+        .build();
+    window.add_action_entries([action_close]);
     window
 }
 
@@ -76,6 +88,7 @@ fn build_list(config: &Config) -> ListBox {
         list_box.emit_move_focus(DirectionType::TabBackward);
         true
     });
+
     let ctrl_p_shortcut = Shortcut::builder()
         .trigger(&ctrl_p_trigger)
         .action(&ctrl_p_action)
@@ -83,6 +96,7 @@ fn build_list(config: &Config) -> ListBox {
     shortcut_controller.add_shortcut(ctrl_p_shortcut);
 
     let ctrl_n_trigger = ShortcutTrigger::parse_string("<Control>n").unwrap();
+
     let ctrl_n_action = CallbackAction::new(move |widget, _| {
         let list_box: &ListBox = widget.downcast_ref().unwrap();
         list_box.emit_move_focus(DirectionType::TabForward);
@@ -93,14 +107,29 @@ fn build_list(config: &Config) -> ListBox {
         .action(&ctrl_n_action)
         .build();
     shortcut_controller.add_shortcut(ctrl_n_shortcut);
-
+    list_box.add_controller(shortcut_controller);
     let controller_key = EventControllerKey::new();
-    controller_key.connect_key_pressed(|k, key, c, modifier_type| {
-        println!("hey, {}, {}", key, c);
+
+    let (sender, receiver) = MainContext::channel::<()>(Priority::DEFAULT);
+    controller_key.connect_key_pressed(move |k, key, c, modifier_type| {
+        // Enter
+        if c == 36 {
+            sender.send(()).unwrap();
+        }
         Propagation::Proceed
     });
     list_box.add_controller(controller_key);
-    list_box.add_controller(shortcut_controller);
+    receiver.attach(
+        None,
+        clone!(@weak list_box => @default-return ControlFlow::Continue, move |_| {
+            let selected_index = list_box.get_index();
+            println!("{}", selected_index);
+
+        list_box.activate_action("win.close", None).unwrap();
+            ControlFlow::Continue
+        }),
+    );
+
     list_box
 }
 
@@ -122,4 +151,24 @@ fn build_entry(label: &str, icon: &str, command: &Command) -> gtk4::Box {
     bx.set_property("name", "entry");
     bx.add_controller(click_controller);
     bx
+}
+
+trait Entries {
+    fn get_index(&self) -> u32;
+}
+
+impl Entries for ListBox {
+    fn get_index(&self) -> u32 {
+        let selected = self.selected_row().unwrap();
+        let mut i: i32 = 0;
+        let mut res = 0;
+        while let Some(found) = self.row_at_index(i) {
+            if selected == found {
+                res = i;
+                break;
+            }
+            i += 1;
+        }
+        res as u32
+    }
 }
